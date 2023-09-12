2e3f9b9d8e42f7e8918a8927c43b55fad9cc1ddf
vivcolvmn <vivcolvmn@gmail.com>
2023-09-11 21:31:10 -0700

    problems5&6

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,5 +1,5 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  return func.bind(this, arg)
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,19 @@
-// Your code here
+const Calculator = require("./02-calculator");
+
+class FancyCalculator extends Calculator {
+	setTotal(newTotal) {
+		this.total = newTotal;
+		return this.total;
+	}
+	modulo(num) {
+		this.total = this.total % num;
+		return this.total;
+	}
+	squared() {
+		this.total = this.total * this.total;
+		return this.total;
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
