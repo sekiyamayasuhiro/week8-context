87cd9f8c0e935db5e21561ffcdf58a0bf752b23a
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2023-09-11 20:46:48 -0700

    problem03

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,5 +1,21 @@
-// Your code here
+class Dog {
+	constructor(name) {
+		this.name = name;
+	};
 
+	static makeJet() {
+		let newDog = new Dog("Jet");
+		return newDog;
+	};
+
+	changeName(newName) {
+		this.name = newName;
+	};
+
+	speak(word) {
+		return `${this.name} says ${word}`
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
